760cdc0c0b345a5c49f1bad23fbf0d4c735a1483
Kevin LaVerdiere <kevinhasfriends@gmail.com>
2017-02-23 17:33:03 -0500

    src + config file

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HelloWorld from './HelloWorld';
+
+const App = () => {
+  return (
+    <div className="HelloWorld">
+      <HelloWorld/>
+      <HelloWorld/>
+    </div>
+  );
+};
+
+export default App;
